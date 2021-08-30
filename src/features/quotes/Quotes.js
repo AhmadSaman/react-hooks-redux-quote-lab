8ed2914666c7867a0bcd7b53346c5d1b58ed98f8
AhmadSaman <ahmad.00079821@gmail.com>
2021-08-30 20:55:20 +0300

    commit tree f6469f6b048c70148371ccdef40699d12dc6a451

--- src/features/quotes/Quotes.js
+++ src/features/quotes/Quotes.js
@@ -1,27 +1,28 @@
 import React from "react";
+import { useSelector } from "react-redux";
 import QuoteCard from "./QuoteCard";
 
 function Quotes() {
-  return (
-    <div>
-      <hr />
-      <div className="row justify-content-center">
-        <h2>Quotes</h2>
-      </div>
-      <hr />
-      <div className="container">
-        <div className="row">
-          <div className="col-md-4">
-            {/*
-              TODO:
+	const quotes = useSelector((state) => state.quotes);
 
-              Render Quotes With QuoteCard component and pass down callback props for removing, upvoting and downvoting quotes
-              */}
-          </div>
-        </div>
-      </div>
-    </div>
-  );
+	return (
+		<div>
+			<hr />
+			<div className="row justify-content-center">
+				<h2>Quotes</h2>
+			</div>
+			<hr />
+			<div className="container">
+				<div className="row">
+					<div className="col-md-4">
+						{quotes.map((quote) => (
+							<QuoteCard key={quote.id} quote={quote} />
+						))}
+					</div>
+				</div>
+			</div>
+		</div>
+	);
 }
 
 export default Quotes;
--- src/index.js
+++ src/index.js
@@ -8,8 +8,8 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import "./index.css";
 
 ReactDOM.render(
-  <Provider store={store}>
-    <App />
-  </Provider>,
-  document.getElementById("root")
+	<Provider store={store}>
+		<App />
+	</Provider>,
+	document.getElementById("root")
 );
